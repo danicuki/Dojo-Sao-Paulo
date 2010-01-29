219824b18236b41d7d8a7676b46a5962d82557a2
Adolfo Sousa <adolfosousa@gmail.com>
2010-01-29 02:51:45 -0200

    refatorando

--- events/campus_party/2010-01-29-water-flow/spec/water_flow_spec.rb
+++ events/campus_party/2010-01-29-water-flow/spec/water_flow_spec.rb
@@ -4,17 +4,17 @@
 
    context "para 2 caixas" do
 
-     it "deve ser [[5,0],[0,5]] com 5 litros iniciais e cano com vazao de 5 l/s" do
+     it "deve ser [[5,0],[0,5]] com 5 litros iniciais e cano com vazao de 5 L/s" do
        water_flow = WaterFlow.new(2, 5, 5)
        water_flow.fluxo.should == [[5,0],[0,5]]
      end
 
-     it "deve ser [[10,0],[0,10]] com 10 litros iniciais e cano com vazao de 10 l/s" do
+     it "deve ser [[10,0],[0,10]] com 10 litros iniciais e cano com vazao de 10 L/s" do
        water_flow = WaterFlow.new(2, 10, 10)
        water_flow.fluxo.should == [[10,0],[0,10]]
      end
 
-     it "deve ser [[10,0],[5,5],[0,10]] com 10 litros iniciais e cano com vazao de 5 l/s" do
+     it "deve ser [[10,0],[5,5],[0,10]] com 10 litros iniciais e cano com vazao de 5 L/s" do
        water_flow = WaterFlow.new(2, 10, 5)
        water_flow.fluxo.should == [[10,0],[5,5],[0,10]]
      end
