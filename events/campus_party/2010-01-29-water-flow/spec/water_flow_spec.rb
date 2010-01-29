850ec8fe81807c3a85ee3f506a563f5638aaee99
Adolfo Sousa <adolfosousa@gmail.com>
2010-01-29 02:45:35 -0200

    red

--- events/campus_party/2010-01-29-water-flow/spec/water_flow_spec.rb
+++ events/campus_party/2010-01-29-water-flow/spec/water_flow_spec.rb
@@ -14,6 +14,11 @@
        water_flow.fluxo.should == [[10,0],[0,10]]
      end
 
+     it "deve ser [[10,0],[5,5],[0,10]] com 10 litros iniciais e cano com vazao de 5 l/s" do
+       water_flow = WaterFlow.new(2, 10, 5)
+       water_flow.fluxo.should == [[10,0],[5,5],[0,10]]
+     end
+
    end
 
 
