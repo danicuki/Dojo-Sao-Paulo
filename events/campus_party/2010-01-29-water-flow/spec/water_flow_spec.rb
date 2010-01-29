909ff3e3c33ef6c945d39f48b5b1a4ca140eb2fb
Adolfo Sousa <adolfosousa@gmail.com>
2010-01-29 02:18:44 -0200

    primeiro teste falhando

--- events/campus_party/2010-01-29-water-flow/spec/water_flow_spec.rb
+++ events/campus_party/2010-01-29-water-flow/spec/water_flow_spec.rb
@@ -2,8 +2,9 @@ require "lib/water_flow"
 
 describe WaterFlow do
 
-  it "deve falhar" do
-    fail
+  it "deve ser [[5,0],[0,5]] para 2 caixas, 5 litros iniciais e cano com vazao de 5 l/s" do
+    water_flow = WaterFlow.new(2, 5, 5)
+    water_flow.fluxo.should == [[5,0],[0,5]]
   end
 
 end
