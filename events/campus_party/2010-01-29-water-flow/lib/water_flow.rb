b3267537c5915441f3de36d96f52b720d9352221
Adolfo Sousa <adolfosousa@gmail.com>
2010-01-29 03:39:53 -0200

    refatorando

--- events/campus_party/2010-01-29-water-flow/lib/water_flow.rb
+++ events/campus_party/2010-01-29-water-flow/lib/water_flow.rb
@@ -7,8 +7,14 @@ class WaterFlow
   end
 
   def fluxo
-    return [[@litros_iniciais,0],[0,@litros_iniciais]] if @litros_iniciais <= @vazao_canos.first
-    [[@litros_iniciais,0],[@litros_iniciais - @vazao_canos.first,@vazao_canos.first],[0,@litros_iniciais]]
+    return [[@litros_iniciais,0],[0,@litros_iniciais]] if @litros_iniciais <= @vazao_canos.primeiro
+    [[@litros_iniciais,0],[@litros_iniciais - @vazao_canos.primeiro,@vazao_canos.primeiro],[0,@litros_iniciais]]
   end
 
+end
+
+class Array
+  def primeiro
+    self.first
+  end
 end
