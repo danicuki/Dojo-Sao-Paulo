969631e4abd8f69718620293de3d892f573bc026
Adolfo Sousa <adolfosousa@gmail.com>
2010-01-29 02:49:24 -0200

    refatorando

--- events/campus_party/2010-01-29-water-flow/lib/water_flow.rb
+++ events/campus_party/2010-01-29-water-flow/lib/water_flow.rb
@@ -2,10 +2,12 @@ class WaterFlow
 
   def initialize(numero_caixas, litros_iniciais, *vazao_canos)
     @litros_iniciais = litros_iniciais
+    @vazao_canos = vazao_canos
   end
 
   def fluxo
-    [[@litros_iniciais,0],[0,@litros_iniciais]]
+    return [[@litros_iniciais,0],[0,@litros_iniciais]] if @litros_iniciais == @vazao_canos[0]
+    [[@litros_iniciais,0],[@litros_iniciais/2,@litros_iniciais/2],[0,@litros_iniciais]]
   end
 
 end
