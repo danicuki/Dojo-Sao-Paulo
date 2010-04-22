07b6424e7aec099b0da05191e431a654d2680a93
Adolfo Sousa <adolfosousa@gmail.com>
2010-04-22 20:34:07 -0300

    arrumem mais

--- locaweb_sp/2010-04-22-numeric-maze/lib/numeric_maze.rb
+++ locaweb_sp/2010-04-22-numeric-maze/lib/numeric_maze.rb
@@ -1,16 +1,15 @@
 class NumericMaze
   def self.solve(from, to)
 
-    return [from, to] if from == to / 2    
     result = [from]
 
     while result.last != to do
       result << result.last * 2
       break if result.size > 100
     end
-    if result.size > 100
-      result = [from]
-    end
+    
+    result = [from] if result.size > 100
+  
     while result.last != to do
       result << result.last + 2
       break if result.size > 100
